05ab1207d60d75451c1ebc441c3cb26291622c59
kacemws <berras.belkacem@gmail.com>
2020-07-09 17:56:20 +0100

    added joi to verify the data according to a schema, added routes and cru operations ( didn't add the delete yet)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
+
+
 
 app.get('/', (req, res)=>{
     res.send("Holà!");
@@ -10,6 +12,10 @@ app.get('/', (req, res)=>{
 app.get('/greet-user/:name', (req, res)=>{
     res.send("Hey " + req.params.name + " Thanks for reaching out ! ");
 });
+
+const quotes = require("./routes/quotes");
+
+app.use('/quotes', quotes);
 
 app.listen(port , ()=>{
     console.log(`listening on port : ${port}`);
---
+++ routes/quotes.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+const Joi = require("@hapi/joi");
+
+let quotes = [
+    {
+        id : 1,
+        quote : "test 1 ",
+        author : "author 1"
+    },
+    {
+        id : 2,
+        quote : "test 2",
+        author : "author 2"
+    },
+    {
+        id : 3,
+        quote : "test 3",
+        author : "author 1"
+    },
+];
+
+
+router.get('/', (req, res) => {
+    res.send(quotes);
+});
+
+router.post('/add',(req, res)=>{
+
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        console.log('Object missing');
+        return res.status(400).send("Empty request");
+    }
+
+    const {error} = verifyQuote(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const aux = quotes.find(c=>c.author == req.body.author && c.quote == req.body.quote);
+
+    if(aux) return res.status(400).send("Already available");
+
+    let quote = {
+        id : quotes.length + 1,
+        quote : req.body.quote,
+        author : req.body.author
+    }
+
+    quotes.push(quote);
+
+    return res.send(quotes);
+
+});
+
+router.patch('/update/:id', (req,res)=>{
+    let aux = quotes.find(c=>c.id === req.params.id);
+    if(!aux) return res.status(400).send("no matching quote is available");
+    aux = req.body;
+    return res.send(quotes);
+});
+
+function verifyQuote(quote){
+
+    const schema = Joi.object(
+        {
+            quote : Joi.string().min(10).required(),
+            author : Joi.string().min(5).required()
+        }
+    );
+
+    return schema.validate(quote);
+}
+
+module.exports = router
